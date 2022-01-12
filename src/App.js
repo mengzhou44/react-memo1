2f19165e6e0327433e809847b56dda2ef1c64827
Meng Zhou <v-menzhou@expediagroup.com>
2022-01-12 13:48:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, memo, useMemo, useCallback } from 'react';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [childNumber, setChildNumber] = useState(0);
+  const [arr, setArr] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  function getLargest() {
+    console.log('call getLargest');
+    return Math.max(...arr);
+  }
+
+  function changeChildNumber() {
+       setChildNumber(childNumber+1)
+  }
+
+  const memorizedCallback = useCallback(() => changeChildNumber(), []);
+  const memorizedLargest = useMemo(() => getLargest(), [arr]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Visited {count} times </h3>
+      <button onClick={() => setCount(count + 1)}> click </button>
+      <button onClick={() => setArr([78, 23, 91])}> set new array </button>
+      <Child number={childNumber} increment={memorizedCallback} />
+      Largest Number
+      {memorizedLargest}
     </div>
   );
-}
+};
+
+const Child = memo(({ number, increment }) => {
+  console.log('render child');
+  return (
+    <div>
+      child  {number}
+      <button onClick={() => increment()}>increment</button>
+    </div>
+  );
+});
 
 export default App;
