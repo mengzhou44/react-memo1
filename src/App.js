717bef52397c1cb62a19fabb92fb89fb3cc284ee
Meng Zhou <v-menzhou@expediagroup.com>
2022-07-06 11:13:03 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
-import React, { useState, memo, useMemo, useCallback } from 'react';
+import React, { useState, useMemo } from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [childNumber, setChildNumber] = useState(0);
-  const [arr, setArr] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  function getLargest() {
-    console.log('call getLargest');
-    return Math.max(...arr);
-  }
-
-  function changeChildNumber() {
-       setChildNumber(childNumber+1)
-  }
-
-  const memorizedCallback = useCallback(() => changeChildNumber(), []);
-  const memorizedLargest = useMemo(() => getLargest(), [arr]);
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i <= 2000000000) i++;
+    return count1 % 2 === 0 ? 'EVEN' : 'ODD';
+  },[count1]);
 
   return (
     <div>
-      <h3>Visited {count} times </h3>
-      <button onClick={() => setCount(count + 1)}> click </button>
-      <button onClick={() => setArr([78, 23, 91])}> set new array </button>
-      <Child number={childNumber} increment={memorizedCallback} />
-      Largest Number
-      {memorizedLargest}
+      <div>
+        <button onClick={() => setCount1(count1 + 1)}> count {count1} </button>
+        <span> {isEven} </span>
+      </div>
+      <div>
+        <button onClick={() => setCount2(count2 + 1)}> count {count2} </button>
+      </div>
     </div>
   );
 };
-
-const Child = memo(({ number, increment }) => {
-  console.log('render child');
-  return (
-    <div>
-      child  {number}
-      <button onClick={() => increment()}>increment</button>
-    </div>
-  );
-});
 
 export default App;
